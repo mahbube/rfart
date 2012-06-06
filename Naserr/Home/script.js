264731d99f447d2c801a9f5c7b51e7448b9a40c6
Naserr <naser0rahmani@gmail.com>
2012-06-07 03:17:11 +0430

    last update!

--- Naserr/Home/script.js
+++ Naserr/Home/script.js
@@ -3,7 +3,17 @@ $(function(){
 	var rmenu = $("div.rmenu");
 	var top_menu = $("div.top_menu");
 	var imgr = $("div.lmenu img");
-	var th = $("div.thumbs");
+
+	var slider = $("div.rmenu div.slider");
+	var li_glass = $("div.thumbs li.glass");
+	var li_boy = $("div.thumbs li.boy");
+	var li_grass = $("div.thumbs li.grass");
+	var li_umberella = $("div.thumbs li.umberella");
+	var li_alone = $("div.thumbs li.alone");
+
+	var logo = $("div.lmenu div.logo");
+
+
 
 	imgr.click(function(){
 		lmenu.css({
@@ -18,6 +28,36 @@ $(function(){
 		imgr.css({
 			'margin':'270px 0px 0px -4px',
 		});
+		logo.css({
+			'display':'none',
+		});
 
 	});
+
+	li_glass.click(function(){
+		slider.css({
+			'background':'url(images/2.jpg)',
+		});
+	});
+	li_boy.click(function(){
+		slider.css({
+			'background':'url(images/3.jpg)',
+		});
+	});
+	li_grass.click(function(){
+		slider.css({
+			'background':'url(images/4.jpg)',
+		});
+	});
+	li_umberella.click(function(){
+		slider.css({
+			'background':'url(images/1.jpg)',
+		});
+	});
+	li_alone.click(function(){
+		slider.css({
+			'background':'url(images/5.jpg)',
+		});
+	});
+
 });
